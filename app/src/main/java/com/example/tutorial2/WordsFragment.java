b8b49f63b101b41999652ea15e9534037a18fe48
leeyunbo <44944031+leeyunbo@users.noreply.github.com>
2019-07-09 22:35:30 +0900

    07-09

---
+++ app/src/main/java/com/example/tutorial2/WordsFragment.java
@@ -0,0 +1,12 @@
+package com.example.tutorial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
