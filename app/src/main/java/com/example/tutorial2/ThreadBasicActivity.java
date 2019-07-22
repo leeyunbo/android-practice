3a6274f567ffde2adb5192a1cb7052b76d8cbddd
leeyunbo <44944031+leeyunbo@users.noreply.github.com>
2019-07-22 16:22:39 +0900

    07-22

---
+++ app/src/main/java/com/example/tutorial2/ThreadBasicActivity.java
@@ -0,0 +1,13 @@
+package com.example.tutorial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThreadBasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread_basic);
+    }
+}
