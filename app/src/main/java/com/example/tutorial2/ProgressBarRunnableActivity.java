f387dd379483725d654ce52a0958f21c9a96dcd5
leeyunbo <44944031+leeyunbo@users.noreply.github.com>
2019-07-22 18:16:34 +0900

    07-22

---
+++ app/src/main/java/com/example/tutorial2/ProgressBarRunnableActivity.java
@@ -0,0 +1,13 @@
+package com.example.tutorial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressBarRunnableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_bar_runnable);
+    }
+}
