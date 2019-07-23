d9817f301cc43b068de387201c5dc9d6427a4a88
leeyunbo <44944031+leeyunbo@users.noreply.github.com>
2019-07-23 18:20:02 +0900

    07-23

---
+++ app/src/main/java/com/example/tutorial2/ImageDownloadMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tutorial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageDownloadMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_download_main);
+    }
+}
